e9a24a52f86076f6f22e36d17dcabd24055730ea
martinjrobins <martinjrobins@gmail.com>
2023-10-03 12:29:27 +0000

    point to diffeq-js lib readme

--- src/help/Help.tsx
+++ src/help/Help.tsx
@@ -14,7 +14,7 @@ const Help = () => {
       <Typography variant="h6" color={'textPrimary'}>
         Help
       </Typography>
-      <p>The code above defines a <a href="https://en.wikipedia.org/wiki/Differential-algebraic_system_of_equations">differential-algebraic system of equations (DAE)</a> of the form <Latex>{"$F(u, \\dot{u}, t) = G(u, t)$"}</Latex> where <Latex>$u$</Latex> is the vector of state variables, <Latex>{"$\\dot{u}$"}</Latex> is the vector of state derivatives, and <Latex>$t$</Latex> is the time variable.</p>
+      <p>The code above is written in a custom domain-specific language (DSL) which defines a <a href="https://en.wikipedia.org/wiki/Differential-algebraic_system_of_equations">differential-algebraic system of equations (DAE)</a> of the form <Latex>{"$F(u, \\dot{u}, t) = G(u, t)$"}</Latex> where <Latex>$u$</Latex> is the vector of state variables, <Latex>{"$\\dot{u}$"}</Latex> is the vector of state derivatives, and <Latex>$t$</Latex> is the time variable.</p>
       <p>The first line <Code>in = [r, k]</Code> labels the parameter inputs (which you can adjust via the sliders on the top right)</p>
       <p>The remaining lines define a series of scalars or vectors. Some names are required to fully define the system, these are <Code>u</Code>, <Code>dudt</Code>, <Code>F</Code>, <Code>G</Code>, and <Code>out</Code>, the remainder are intermediate variables used to define the system. A variable must be defined before it is used in an equation for another variable.</p>
       <p>for example:</p>
@@ -24,7 +24,7 @@ const Help = () => {
         <li><Code>{"out_i { y }"}</Code> defines the output vector (shown in the plot to the right). This vector only has one element with a value <Code>y</Code> (defined previously as the first element of the state vector).</li>
       </ul>
       <p>Try to change the equations solved and then hit the save button in the top left hand corner of your screen to compile the system. If there are no errors you will be able to adjust the input paramters via the sliders on the top right, and see the solution of your DAE system in the plot on the right. If you need to adjust the time duration to solve the system over, use the "max time" input field below the sliders</p>
-      <p>For more information, please contact the author of this page, Martin Robinson (<a href="https://github.com/martinjrobins">github</a>, <a href="mailto:martinjrobins@gmail.com">email</a>).</p>
+      <p>For more information about the DSL or how it is compiled and solved, please see the `diffeq-js` library <a href="https://github.com/martinjrobins/diffeq-js">README</a>, or contact the author of this page, Martin Robinson (<a href="https://github.com/martinjrobins">github</a>, <a href="mailto:martinjrobins@gmail.com">email</a>).</p>
       
     </Box>
   );
